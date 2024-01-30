920e442418bd3f2c7f1bcec7668c17176f4ade41
viskiarditia <viskiarditia@gmail.com>
2024-01-30 14:35:59 +0700

    general information selesai

--- src/test/java/step_definitions/JobOrder_Request.java
+++ src/test/java/step_definitions/JobOrder_Request.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -25,12 +26,13 @@ public class JobOrder_Request {
     @And("User Click menu Job Order")
     public void userClickMenuJobOrder() throws InterruptedException {
         Actions actions = new Actions(webDriver);
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
 
-        WebElement clickMenuHamburger = webDriver.findElement(By.xpath("//div[@class='hidden ml-24 lg:flex flex-1']//div[3]/li[@class='MuiButtonBase-root MuiListItem-root MuiListItem-gutters MuiListItem-padding MuiListItem-button fuse-list-item relative level-0 muiltr-569kmf']"));
+        WebElement clickMenuHamburger = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='hidden ml-24 lg:flex flex-1']//div[3]/li[@class='MuiButtonBase-root MuiListItem-root MuiListItem-gutters MuiListItem-padding MuiListItem-button fuse-list-item relative level-0 muiltr-569kmf']")));
         actions.moveToElement(clickMenuHamburger).click().perform();
         Thread.sleep(3000);
 
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
         WebElement clickMenuJobOrder = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".muiltr-1zy08t:nth-child(1) > .MuiListItemText-root")));
         clickMenuJobOrder.click();
         Thread.sleep(2000);
@@ -38,7 +40,8 @@ public class JobOrder_Request {
 
     @Then("User Add New Data Job Order Request")
     public void userAddNewDataJobOrderRequest() throws InterruptedException {
-        WebElement AddNewData = webDriver.findElement(By.xpath("//a[.='Add New']"));
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebElement AddNewData = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[.='Add New']")));
         AddNewData.click();
         Thread.sleep(2000);
     }
@@ -163,4 +166,94 @@ public class JobOrder_Request {
         inputTestDataReason("//input[@name='estimated_salary']", data);
         inputTestDataReason("//input[@name='estimated_salary_max']", data);
     }
+
+
+    private void inputTestDataSkillCertification(String xpath, String data) {
+        WebElement element = webDriver.findElement(By.xpath(xpath));
+
+        // Tunggu hingga elemen menjadi interaktif (maksimum 10 detik)
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+
+        element.click();
+        element.sendKeys(data);
+    }
+
+    @When("User Click every field in Skill Certification")
+    public void userClickEveryFieldInSkillCertification() throws InterruptedException {
+        WebElement skillCertification= webDriver.findElement(By.xpath("//p[.='Skill/Certification *Please complete the following data']"));
+        skillCertification.click();
+        Thread.sleep(2000);
+
+        WebElement typeSkillCertification= webDriver.findElement(By.xpath("//input[@id='combo-box-Type Skill/Certification']"));
+        typeSkillCertification.click();
+        WebElement nameSkillCertification= webDriver.findElement(By.xpath("//div[@class='grid grid-cols-1 gap-14 md:grid-cols-12 items-center']//input[@class='MuiInputBase-input MuiOutlinedInput-input muiltr-zwrapf']"));
+        nameSkillCertification.click();
+        WebElement skalaLevel= webDriver.findElement(By.xpath("//input[@id='combo-box-Skala Level']"));
+        skalaLevel.click();
+    }
+
+    @Then("Input any Test in every Field Skill Certification {string}")
+    public void inputAnyTestInEveryFieldSkillCertification(String data) {
+        inputTestDataSkillCertification("//input[@id='combo-box-Type Skill/Certification']", data);
+        inputTestDataSkillCertification("//div[@class='grid grid-cols-1 gap-14 md:grid-cols-12 items-center']//input[@class='MuiInputBase-input MuiOutlinedInput-input muiltr-zwrapf']", data);
+        inputTestDataSkillCertification("//input[@id='combo-box-Skala Level']", data);
+    }
+
+    @When("User Input All Field Form")
+    public void userInputAllFieldForm() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebElement generalInformation = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//div[@class='undefined file-details p-24']//div[@class='MuiButtonBase-root MuiAccordionSummary-root MuiAccordionSummary-gutters muiltr-1iji0d4']/div[.='General Information *Please complete the following data']")));
+        generalInformation.click();
+        Thread.sleep(2000);
+
+        WebElement scaleOfNeeds = webDriver.findElement(By.xpath("//input[@id='combo-box-Scale of Needs']"));
+        scaleOfNeeds.click();
+        WebElement dropDownOption1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Scale of Needs-option-1")));
+        dropDownOption1.click();
+        scaleOfNeeds.click();
+
+        WebElement company = webDriver.findElement(By.xpath("//input[@id='combo-box-Company']"));
+        company.click();
+        WebElement dropDownOption2 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@id='combo-box-Company-option-1']")));
+        dropDownOption2.click();
+        company.sendKeys(Keys.ENTER);
+
+        WebElement jobRequired = webDriver.findElement(By.xpath("//input[@id='combo-box-Job Required']"));
+        jobRequired.click();
+        WebElement dropDownOption3 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@id='combo-box-Job Required-option-54']")));
+        dropDownOption3.click();
+        jobRequired.sendKeys(Keys.ENTER);
+
+        WebElement department = webDriver.findElement(By.xpath("//input[@id='combo-box-Department']"));
+        department.click();
+        WebElement dropDownOption4 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@id='combo-box-Department-option-5']")));
+        dropDownOption4.click();
+        department.sendKeys(Keys.ENTER);
+
+        WebElement location = webDriver.findElement(By.xpath("//input[@name='location']"));
+        location.click();
+        location.sendKeys("bogor");
+        location.sendKeys(Keys.ENTER);
+
+        WebElement totalNeeded = webDriver.findElement(By.xpath("//input[@name='total_needed']"));
+        totalNeeded.click();
+        totalNeeded.clear();
+        totalNeeded.sendKeys("1");
+
+        WebElement datePicker = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@class='MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeEnd MuiIconButton-sizeMedium muiltr-1r7lzhg']")));
+        datePicker.click();
+        // Pilih tahun pada date picker
+        WebElement yearDropdown = datePicker.findElement(By.xpath("//button[@class='MuiButtonBase-root MuiIconButton-root MuiIconButton-sizeSmall MuiPickersCalendarHeader-switchViewButton muiltr-1aoxnv6']"));
+        yearDropdown.click();
+        WebElement yearDropdown1 = datePicker.findElement(By.xpath("//button[@class='PrivatePickersYear-yearButton Mui-selected muiltr-ow191y']"));
+        yearDropdown1.click();
+        WebElement monthDropdown = datePicker.findElement(By.xpath("//button[@class='MuiButtonBase-root MuiIconButton-root MuiIconButton-edgeStart MuiIconButton-sizeSmall MuiPickersArrowSwitcher-button muiltr-hr1ny1']"));
+        monthDropdown.click();
+        WebElement dayButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[.='2']")));
+        dayButton.click();
+        Thread.sleep(5000);
+    }
+
+
 }
