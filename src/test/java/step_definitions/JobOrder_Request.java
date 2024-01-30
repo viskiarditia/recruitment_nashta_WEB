6dfb454b368c2972ab9f51562831173dd52a2676
viskiarditia <viskiarditia@gmail.com>
2024-01-30 15:27:55 +0700

    add data valid job request

--- src/test/java/step_definitions/JobOrder_Request.java
+++ src/test/java/step_definitions/JobOrder_Request.java
@@ -203,6 +203,8 @@ public class JobOrder_Request {
     @When("User Input All Field Form")
     public void userInputAllFieldForm() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+
+        //general information
         WebElement generalInformation = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//div[@class='undefined file-details p-24']//div[@class='MuiButtonBase-root MuiAccordionSummary-root MuiAccordionSummary-gutters muiltr-1iji0d4']/div[.='General Information *Please complete the following data']")));
         generalInformation.click();
         Thread.sleep(2000);
@@ -252,6 +254,118 @@ public class JobOrder_Request {
         monthDropdown.click();
         WebElement dayButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[.='2']")));
         dayButton.click();
+
+        //reason of request
+        WebElement reasonOfRequest = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p[.='Reason for Request *Please complete the following data']")));
+        reasonOfRequest.click();
+        Thread.sleep(2000);
+
+        WebElement procurmentReason = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='reason_desc']")));
+        procurmentReason.click();
+        procurmentReason.sendKeys("ahli");
+        procurmentReason.sendKeys(Keys.ENTER);
+
+        WebElement projectType = webDriver.findElement(By.xpath("//input[@id='combo-box-Project Type']"));
+        projectType.click();
+        WebElement dropDownOption5 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Project Type-option-0")));
+        dropDownOption5.click();
+
+        WebElement employeeStatus = webDriver.findElement(By.xpath("//input[@id='combo-box-Employee Status']"));
+        employeeStatus.click();
+        WebElement dropDownOption6 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Employee Status-option-0")));
+        dropDownOption6.click();
+
+
+        WebElement positionLevel = webDriver.findElement(By.xpath("//input[@id='combo-box-Position Level']"));
+        positionLevel.click();
+        WebElement dropDownOption7 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Position Level-option-4")));
+        dropDownOption7.click();
+
+        WebElement age = webDriver.findElement(By.xpath("//input[@id='combo-box-Age']"));
+        age.click();
+        WebElement dropDownOption8 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Age-option-1")));
+        dropDownOption8.click();
+
+        WebElement otherAge = webDriver.findElement(By.xpath("//input[@name='age_remarks']"));
+        otherAge.click();
+
+        WebElement fullfillmentSource = webDriver.findElement(By.xpath("//input[@id='combo-box-Fulfillment Source']"));
+        fullfillmentSource.click();
+        WebElement dropDownOption9 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Fulfillment Source-option-0")));
+        dropDownOption9.click();
+
+        WebElement selectionTest = webDriver.findElement(By.xpath("//input[@id='combo-box-Selection Test']"));
+        selectionTest.click();
+        WebElement dropDownOption10 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Selection Test-option-0")));
+        dropDownOption10.click();
+        selectionTest.click();
+
+        WebElement boxGender = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='combo-box-Gender']")));
+        boxGender.click();
+        WebElement dropDownOption11 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Gender-option-0")));
+        dropDownOption11.click();
+        boxGender.click();
+
+        WebElement educationalLevel = webDriver.findElement(By.xpath("//input[@id='combo-box-Education Level']"));
+        educationalLevel.click();
+        WebElement dropDownOption12 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Education Level-option-2")));
+        dropDownOption12.click();
+
+        WebElement workExperience = webDriver.findElement(By.xpath("//input[@id='combo-box-Work Experience']"));
+        workExperience.click();
+        WebElement dropDownOption13 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Work Experience-option-2")));
+        dropDownOption13.click();
+
+        WebElement estimated_salary = webDriver.findElement(By.xpath("//input[@name='estimated_salary']"));
+        estimated_salary.click();
+        estimated_salary.sendKeys(Keys.END);
+        estimated_salary.clear();
+        estimated_salary.sendKeys("7000000");
+
+        WebElement estimatedSalaryMax = webDriver.findElement(By.xpath("//input[@name='estimated_salary_max']"));
+        estimatedSalaryMax.click();
+        estimatedSalaryMax.sendKeys(Keys.END);
+        estimatedSalaryMax.clear();
+        estimatedSalaryMax.sendKeys("10000000");
+
+        //Job Description
+        WebElement jobDescription = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p[.='Job Description *Please complete the following data']")));
+        jobDescription.click();
+        Thread.sleep(2000);
+
+        WebElement titleJobDescription = webDriver.findElement(By.xpath("//div[@class='grid grid-cols-1 gap-14 ']//input[@class='MuiInputBase-input MuiOutlinedInput-input muiltr-zwrapf']"));
+        titleJobDescription.click();
+        titleJobDescription.sendKeys("Quality Assurance Engineer");
+
+        WebElement notes = webDriver.findElement(By.xpath("//textarea[@name='job_desc_notes']"));
+        notes.click();
+        notes.sendKeys("What is Lorem Ipsum?\n" +
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+
+        //Skill/Certification
+        WebElement skillcertification = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p[.='Skill/Certification *Please complete the following data']")));
+        skillcertification.click();
+        Thread.sleep(2000);
+
+        WebElement typeSkillCertification= webDriver.findElement(By.xpath("//input[@id='combo-box-Type Skill/Certification']"));
+        typeSkillCertification.click();
+        WebElement dropDownOption14 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Type Skill/Certification-option-0")));
+        dropDownOption14.click();
+
+        WebElement nameSkillCertification= webDriver.findElement(By.xpath("//div[@class='grid grid-cols-1 gap-14 md:grid-cols-12 items-center']//input[@class='MuiInputBase-input MuiOutlinedInput-input muiltr-zwrapf']"));
+        nameSkillCertification.click();
+        nameSkillCertification.sendKeys("Coursera");
+
+        WebElement skalaLevel= webDriver.findElement(By.xpath("//input[@id='combo-box-Skala Level']"));
+        skalaLevel.click();
+        WebElement dropDownOption15 = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("combo-box-Skala Level-option-1")));
+        dropDownOption15.click();
+
+
+        WebElement SUBMIT= webDriver.findElement(By.xpath("//button[@class='MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedRctBlue MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-root MuiButton-contained MuiButton-containedRctBlue MuiButton-sizeMedium MuiButton-containedSizeMedium px-20 whitespace-nowrap px-28 ml-10 muiltr-isfyrj']"));
+        SUBMIT.click();
+
         Thread.sleep(5000);
     }
 
